aed1313a20245d19416a039141bdfba0184debd4
bensb1 <162532@chrome.escg.ac.uk>
2021-10-13 11:55:28 +0100

    maded it four in a row to win

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -51,78 +51,80 @@ public class GameController : MonoBehaviour
     public void EndTurn()
     {
         moveCount++;
-        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide && buttonList[9].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide && buttonList[11].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide && buttonList[13].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide && buttonList[15].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide && buttonList[16].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide && buttonList[17].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        else if (buttonList[23].text == playerSide && buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[2].text == playerSide && buttonList[9].text == playerSide && buttonList[10].text == playerSide)
+        else if (buttonList[1].text == playerSide && buttonList[2].text == playerSide && buttonList[9].text == playerSide && buttonList[10].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[5].text == playerSide && buttonList[11].text == playerSide && buttonList[12].text == playerSide)
+        else if (buttonList[4].text == playerSide && buttonList[5].text == playerSide && buttonList[11].text == playerSide && buttonList[12].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[8].text == playerSide && buttonList[13].text == playerSide && buttonList[14].text == playerSide)
+        else if (buttonList[7].text == playerSide && buttonList[8].text == playerSide && buttonList[13].text == playerSide && buttonList[14].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+       /* else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[15].text == playerSide && buttonList[16].text == playerSide && buttonList[17].text == playerSide)
+       */
+        else if (buttonList[15].text == playerSide && buttonList[16].text == playerSide && buttonList[17].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[17].text == playerSide && buttonList[18].text == playerSide && buttonList[19].text == playerSide)
+        else if (buttonList[16].text == playerSide && buttonList[17].text == playerSide && buttonList[18].text == playerSide && buttonList[19].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[9].text == playerSide && buttonList[11].text == playerSide && buttonList[13].text == playerSide)
+        else if (buttonList[9].text == playerSide && buttonList[11].text == playerSide && buttonList[13].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[11].text == playerSide && buttonList[13].text == playerSide && buttonList[18].text == playerSide)
+        else if (buttonList[9].text == playerSide && buttonList[11].text == playerSide && buttonList[13].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[10].text == playerSide && buttonList[12].text == playerSide && buttonList[14].text == playerSide)
+        else if (buttonList[24].text == playerSide && buttonList[10].text == playerSide && buttonList[12].text == playerSide && buttonList[14].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[12].text == playerSide && buttonList[14].text == playerSide && buttonList[19].text == playerSide)
+        else if (buttonList[10].text == playerSide && buttonList[12].text == playerSide && buttonList[14].text == playerSide && buttonList[19].text == playerSide)
         {
             GameOver(playerSide);
         }
+        /*
            else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
             GameOver(playerSide);
@@ -130,103 +132,110 @@ public class GameController : MonoBehaviour
         {
             GameOver(playerSide);
         }
-        else if (buttonList[2].text == playerSide && buttonList[11].text == playerSide && buttonList[14].text == playerSide)
+        */
+        else if (buttonList[21].text == playerSide && buttonList[2].text == playerSide && buttonList[11].text == playerSide && buttonList[14].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[1].text == playerSide && buttonList[5].text == playerSide && buttonList[13].text == playerSide)
+        else if (buttonList[20].text == playerSide && buttonList[1].text == playerSide && buttonList[5].text == playerSide && buttonList[13].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[5].text == playerSide && buttonList[13].text == playerSide && buttonList[19].text == playerSide)
+        else if (buttonList[1].text == playerSide && buttonList[5].text == playerSide && buttonList[13].text == playerSide && buttonList[19].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[3].text == playerSide && buttonList[7].text == playerSide && buttonList[17].text == playerSide)
+       /* else if (buttonList[3].text == playerSide && buttonList[7].text == playerSide && buttonList[17].text == playerSide)
         {
             GameOver(playerSide);
         }
+       
         else if (buttonList[17].text == playerSide && buttonList[13].text == playerSide && buttonList[12].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[16].text == playerSide && buttonList[8].text == playerSide && buttonList[11].text == playerSide)
+       */
+        else if (buttonList[16].text == playerSide && buttonList[8].text == playerSide && buttonList[11].text == playerSide && buttonList[10].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[8].text == playerSide && buttonList[11].text == playerSide && buttonList[10].text == playerSide)
+        else if (buttonList[8].text == playerSide && buttonList[11].text == playerSide && buttonList[10].text == playerSide && buttonList[16].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[15].text == playerSide && buttonList[7].text == playerSide && buttonList[5].text == playerSide)
+        else if (buttonList[15].text == playerSide && buttonList[7].text == playerSide && buttonList[5].text == playerSide && buttonList[9].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[7].text == playerSide && buttonList[5].text == playerSide && buttonList[9].text == playerSide)
+        else if (buttonList[7].text == playerSide && buttonList[5].text == playerSide && buttonList[9].text == playerSide && buttonList[24].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[4].text == playerSide && buttonList[8].text == playerSide && buttonList[18].text == playerSide)
+        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[16].text == playerSide && buttonList[17].text == playerSide && buttonList[18].text == playerSide)
+        else if (buttonList[15].text == playerSide && buttonList[16].text == playerSide && buttonList[17].text == playerSide && buttonList[18].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[20].text == playerSide && buttonList[21].text == playerSide && buttonList[22].text == playerSide)
+        else if (buttonList[20].text == playerSide && buttonList[21].text == playerSide && buttonList[22].text == playerSide && buttonList[23].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[22].text == playerSide && buttonList[23].text == playerSide && buttonList[24].text == playerSide)
+        else if (buttonList[21].text == playerSide && buttonList[22].text == playerSide && buttonList[23].text == playerSide && buttonList[24].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[20].text == playerSide && buttonList[0].text == playerSide && buttonList[3].text == playerSide)
+        else if (buttonList[20].text == playerSide && buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[21].text == playerSide && buttonList[1].text == playerSide && buttonList[4].text == playerSide)
+        else if (buttonList[21].text == playerSide && buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[22].text == playerSide && buttonList[2].text == playerSide && buttonList[5].text == playerSide)
+        else if (buttonList[22].text == playerSide && buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[23].text == playerSide && buttonList[9].text == playerSide && buttonList[11].text == playerSide)
+        else if (buttonList[23].text == playerSide && buttonList[9].text == playerSide && buttonList[11].text == playerSide && buttonList[13].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[24].text == playerSide && buttonList[10].text == playerSide && buttonList[12].text == playerSide)
+        else if (buttonList[24].text == playerSide && buttonList[10].text == playerSide && buttonList[12].text == playerSide && buttonList[14].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[21].text == playerSide && buttonList[1].text == playerSide && buttonList[5].text == playerSide)
+        else if (buttonList[21].text == playerSide && buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[21].text == playerSide && buttonList[2].text == playerSide && buttonList[11].text == playerSide)
+        else if (buttonList[21].text == playerSide && buttonList[2].text == playerSide && buttonList[11].text == playerSide && buttonList[14].text == playerSide)
         {
             GameOver(playerSide);
         }
+        /*
         else if (buttonList[22].text == playerSide && buttonList[9].text == playerSide && buttonList[12].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[24].text == playerSide && buttonList[9].text == playerSide && buttonList[5].text == playerSide)
+        */
+        else if (buttonList[24].text == playerSide && buttonList[9].text == playerSide && buttonList[5].text == playerSide && buttonList[7].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[23].text == playerSide && buttonList[2].text == playerSide && buttonList[4].text == playerSide)
+        else if (buttonList[23].text == playerSide && buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
             GameOver(playerSide);
         }
+        /*
         else if (buttonList[22].text == playerSide && buttonList[1].text == playerSide && buttonList[3].text == playerSide)
         {
             GameOver(playerSide);
         }
-        else if (buttonList[20].text == playerSide && buttonList[1].text == playerSide && buttonList[5].text == playerSide)
+        */
+        else if (buttonList[20].text == playerSide && buttonList[1].text == playerSide && buttonList[5].text == playerSide && buttonList[13].text == playerSide)
         {
             GameOver(playerSide);
         }
